d80e998ddd8c1fd81bef702ca46ec58f053c9a7f
KeotshepileMaje <125873781+KeotshepileMaje@users.noreply.github.com>
2024-04-23 10:53:19 +0200

    Added footer

---
+++ app/components/footer/Footer.jsx
@@ -0,0 +1,58 @@
+import Link from "next/link";
+import Container from "../Container";
+import FooterList from "./FooterList";
+
+const Footer = () => {
+  return (
+    <div className="p-4">
+      <Container>
+        <div className=" border-2 border-t-8 border-violet-800 p-4">
+          <div className="flex justify-between">
+            <FooterList>
+              <h3>About</h3>
+              <Link href="#"></Link>
+              <Link href="#"></Link>
+              <Link href="#"></Link>
+              <Link href="#"></Link>
+              <Link href="#"></Link>
+            </FooterList>
+            <FooterList>
+              <h3>Rent a Car</h3>
+              <Link href="#">Request a Quote</Link>
+              <Link href="#">Rent a vehicle</Link>
+              <Link href="#">Special Offers</Link>
+            </FooterList>
+            <FooterList>
+              <h3>Social Media</h3>
+              <Link href="#">Twitter</Link>
+              <Link href="#">LinkedIn</Link>
+              <Link href="#">TikTok</Link>
+              <Link href="#">Instagram</Link>
+            </FooterList>
+          </div>
+          <div
+            className="
+            flex 
+            items-center
+            justify-between 
+            gap-4
+            "
+          >
+            <div>Intergrity & Compliance</div>
+            <div>|</div>
+            <div>Legal</div>
+            <div>|</div>
+            <div>Manage cookies</div>
+            <div>|</div>
+            <div>Privacy & Safety</div>
+          </div>
+          <div className="w-fit border-t-8 border-violet-800 -pt-4">
+            &copy; Unlimited Autos 2024
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Footer;
