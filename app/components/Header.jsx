a05488ed86cbdbed90d5076bac9a72396f7b4253
KeotshepileMaje <125873781+KeotshepileMaje@users.noreply.github.com>
2024-05-01 09:09:46 +0200

    Added border

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -5,19 +5,22 @@ import BackgroundCarousel from "./carousel/BackgroundCarousel";
 const Header = () => {
     return (
       <div>
-        <BackgroundCarousel>
-          <div className="bg-slate-200 mb-8 md:mb-0 text-center">
-            <h1 className="text-4xl md:text-6xl text-white mb-4">
-              DRIVE WITH THE BEST
-            </h1>
-            <div className="text-lg md:text-xl text-white md-2">
-              BEST SERVICE
+        <div className="border-b-2 border-merigold">
+          <BackgroundCarousel>
+            <div className="bg-slate-200 mb-8 md:mb-0 text-center">
+              <h1 className="text-4xl md:text-6xl text-white mb-4">
+                DRIVE WITH THE BEST
+              </h1>
+              <div className="text-lg md:text-xl text-white md-2">
+                BEST SERVICE
+              </div>
+              <p className="text-2xl md:text-5xl text-yellow-400 font-bold">
+                BEST PRICES
+              </p>
             </div>
-            <p className="text-2xl md:text-5xl text-yellow-400 font-bold">
-              BEST PRICES
-            </p>
-          </div>
-        </BackgroundCarousel>
+          </BackgroundCarousel>
+        </div>
+
         <Container>
           <div className="h-[30vh] px-4">
             <BookingsCard />
