e4377768834e9fa468234c9c641cedfee43487bd
KeotshepileMaje <125873781+KeotshepileMaje@users.noreply.github.com>
2024-05-01 09:09:01 +0200

    Added Temporary bg color for outline button

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -25,7 +25,7 @@ const Button = ({
         items=center
         justify-center
         gap-2
-        ${outline ? "bg-white" : "bg-slate-700"}
+        ${outline ? "bg-white hover:bg-emerald-600" : "bg-slate-700"}
         ${outline ? "text-slate" : "text-white"}
         ${small ? "text-sm font-light" : "text-md font-semibold"}
         ${small ? "py-1 px-2 border-[1px]" : "py-3 px-4 border-2"}
