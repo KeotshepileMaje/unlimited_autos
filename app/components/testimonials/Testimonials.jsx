6b8c2e5f18177bcf74d0a9fcdca7565eda3ad2b0
KeotshepileMaje <125873781+KeotshepileMaje@users.noreply.github.com>
2024-05-01 09:09:59 +0200

    Added heading

--- app/components/testimonials/Testimonials.jsx
+++ app/components/testimonials/Testimonials.jsx
@@ -2,14 +2,18 @@ import Image from "next/image";
 import Container from "../Container";
 import { truncateText } from "@/utils/trancate";
 import Testimony from "./Testimony";
+import Heading from "../Heading";
 
 const Testimonials = () => {
   return (
     <Container>
+      <Heading
+      title='What People Say About Us'
+      />
       <div className="flex justify-center gap-2">
         <Testimony />
-      <Testimony />
-      <Testimony />
+        <Testimony />
+        <Testimony />
       </div>
     </Container>
   );
