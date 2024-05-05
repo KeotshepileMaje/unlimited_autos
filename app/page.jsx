0d8834d8d465d7f0c3b1ee60bf319d2553a36493
KeotshepileMaje <125873781+KeotshepileMaje@users.noreply.github.com>
2024-05-05 16:01:06 +0200

    Added more components

--- app/page.jsx
+++ app/page.jsx
@@ -3,20 +3,34 @@ import CarsList from "./components/CarsList";
 import AboutUs from "./components/AboutUs";
 import BackgroundCarousel from "./components/carousel/BackgroundCarousel";
 import Header from "./components/Header";
-import GoogleMaps from "./components/GoogleMaps"
-
+import GoogleMaps from "./components/GoogleMaps";
+import BrandLogos from "./components/BrandLogos";
 
 export default function Home() {
   return (
     <div>
+
       
-      <Header/>
+      <Header />
+      <BrandLogos />
       <main>
         <AboutUs />
-      <CarsList />
-      <Testimonials />
-      <GoogleMaps/>
+        <CarsList />
+        <Testimonials />
+        <section className="mx-auto">
+          <div
+            className="
+          bg-[url('/car-show.jpg')] bg-fixed bg-cover bg-center h-96 w-full 
+          pl-4 pt-4 
+          flex justify-center items-center
+          text-6xl text-white"
+          >
+            BEST OF LUXURY
+          </div>
+        </section>
+        <GoogleMaps />
       </main>
+
     </div>
   );
 }
