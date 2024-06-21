a63786674b27362dd0f378204d92ecae43fb3345
KeotshepileMaje <125873781+KeotshepileMaje@users.noreply.github.com>
2024-06-21 14:35:32 +0200

    add tailwind utils

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+
 const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 module.exports = {
   content: [
@@ -14,36 +18,44 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      black: colors.black,
-      white: colors.white,
-      gray: colors.gray,
-      emerald: colors.emerald,
-      indigo: colors.indigo,
-      yellow: colors.yellow,
-      slate: colors.slate,
-      hazelWood: "#bdb7ac",
-      merigold: {
-        100: "#FCAE1E",
-        200: "#FFAD4A",
-        300: "#BF8237",
-        400: "#805725",
-        500: "#402B12",
+      colors: {
+        transparent: "transparent",
+        hazelWood: "#bdb7ac",
+        merigold: {
+          100: "#FCAE1E",
+          200: "#FFAD4A",
+          300: "#BF8237",
+          400: "#805725",
+          500: "#402B12",
+        },
+        navy: {
+          100: "#3D5AFF",
+          200: "#2237B3",
+        },
       },
-      navy: {
-        100: '#3D5AFF',
-        200:'#2237B3',
-      }
+      animation: {
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
 
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
 
-// emerald: {
-//         100: "#57FFDB",
-//         200: "#46B39B",
-//       },
+  addBase({
+    ":root": newVars,
+  });
+}
